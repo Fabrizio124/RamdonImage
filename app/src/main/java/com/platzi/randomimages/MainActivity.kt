5e1ec160e19bf15cd80265e191baca3230df2bc7
Trilador123 <fabrix_fvz@hotmail.com>
2021-04-19 12:47:42 -0600

    proyecto 1

---
+++ app/src/main/java/com/platzi/randomimages/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.platzi.randomimages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.squareup.picasso.Picasso
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var imagePhoto: ImageView = findViewById(R.id.imagePhoto)
+        var imagePhoto2: ImageView = findViewById(R.id.imagePhoto2)
+        var btnGetUrlImage: Button = findViewById(R.id.btnUrlImage)
+        // var model: MainActivityViewModel = MainActivityViewModel()
+
+        var model = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        var urlImage: MutableLiveData<String?>? = model.callUrlImage()
+
+        urlImage?.observe(this, Observer{
+            print("Se ejecuta si la url sufre un cambio")
+
+            Picasso.get().load(it).into(imagePhoto)
+            Picasso.get().load(it).into(imagePhoto2)
+        })
+
+        btnGetUrlImage.setOnClickListener {
+            model.randomNumbersUrl()
+        }
+
+    }
+}
